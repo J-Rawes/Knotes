d0cfc38972ade16e480f438cdc8045c5f2907008
ryan-ulgiati <144563996+ryan-ulgiati@users.noreply.github.com>
2025-02-19 17:08:56 -0500

    Update register.js

--- FrontEnd/register.js
+++ FrontEnd/register.js
@@ -1,7 +1,9 @@
+const client = require('./server.js'); //Grab client info from server.js
+
 function login() {
 
-  var username = document.logIn.user.value;
-  var password = document.logIn.password.value;
+  var username = document.logIn.user.value; //user's username
+  var password = document.logIn.password.value; //user's password
   var password2 = document.logIn.password2.value;
   var conditions = ["\\","<",">","|","/","=","&","#"];
 
@@ -23,9 +25,28 @@ function login() {
   } else if (username.length === 0 || password.length === 0){
     document.getElementById("message").style.color = "#f56476";
     document.getElementById("message").innerHTML = "Please fill in all fields";
-  } else {
+  } else { //User accepted
+    sendToDB(username, password); //ACTUALLY SEND TO DB
     localStorage.setItem("username", username);
-    location.href = "homepage.html";
+    location.href = "homepage.html";  
   }
   return false;
 }
+
+function sendToDB(uname, pword) {
+  fetch('/register', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username: uname, password: pword })
+  })
+  .then(response => response.text())
+  .then(data => {
+      console.log(data);
+      document.getElementById("message").innerText = data;
+  })
+  .catch(error => console.error('Error:', error));
+}
+
+
