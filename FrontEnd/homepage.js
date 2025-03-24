94d92bfe3a4e24287e4364b13908f72b0e0de131
connordod <cd21ly@brocku.ca>
2025-03-23 22:14:32 -0400

    Update homepage.js
    read username from cookie

--- FrontEnd/homepage.js
+++ FrontEnd/homepage.js
@@ -1,1 +1,7 @@
 document.getElementById("userN").innerHTML = localStorage.getItem("username");
+
+document.addEventListener("DOMContentLoaded", function () {
+   let decodedCookie = decodeURIComponent(document.cookie);
+   let ca = decodedCookie.split(';');
+   username = ca[1];
+}
