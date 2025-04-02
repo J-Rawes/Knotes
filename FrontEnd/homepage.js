a3df818e6932b524024d17832523764ccc1081ec
ryan-ulgiati <144563996+ryan-ulgiati@users.noreply.github.com>
2025-04-02 15:08:32 -0400

    Add files via upload

--- FrontEnd/homepage.js
+++ FrontEnd/homepage.js
@@ -1,7 +1,7 @@
-document.getElementById("userN").innerHTML = localStorage.getItem("username");
-
 document.addEventListener("DOMContentLoaded", function () {
    let decodedCookie = decodeURIComponent(document.cookie);
    let ca = decodedCookie.split(';');
    username = ca[1];
+
+   document.getElementById("userN").innerHTML = username || "Unknown User";
 });
